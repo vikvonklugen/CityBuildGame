5cfadb7a4977294f75fa4329091e57bce81a0813
Ole Jakob Schjøth <ole_jakob94@hotmail.com>
2020-05-30 01:49:00 +0200

    Added project folders. Modified project settings to build for 1024 x 768 resulotion, added simple UI templates for buildings. Added UI test scene for creating prefabs for UI. Added a simple map-generator.

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private int rows = 6;
+    [SerializeField]
+    private int cols = 6;
+
+    [SerializeField]
+    private float tileSize = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateGrid();
+        Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+        //Camera.current.WorldToScreenPoint(new Vector3(transform.position.x, transform.position.y, -10));
+    }
+
+    private void GenerateGrid ()
+    {
+        GameObject referenceTile = (GameObject)Instantiate(Resources.Load("placeholderTile"));
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+
+                GameObject tile = Instantiate(referenceTile, transform) as GameObject;
+                float posX = col * tileSize;
+                float posY = row * -tileSize; // <-- cartesian position system
+                tile.transform.position = new Vector2(posX, posY);
+            }
+        }
+
+        Destroy(referenceTile);
+
+        float gridWidth = cols * tileSize;
+        float gridHeight = rows * tileSize;
+        transform.position = new Vector2(gridWidth / 2 + tileSize / 2, gridHeight / 2 - tileSize / 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
