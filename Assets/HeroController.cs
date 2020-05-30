2d5bf20de8e07d5cb3e8b16bfc3f549da8ff99d6
Ole Jakob Schjøth <ole_jakob94@hotmail.com>
2020-05-31 01:49:39 +0200

    Moved some files

--- Assets/HeroController.cs
+++ Assets/HeroController.cs
@@ -11,14 +11,29 @@ public class HeroController : MonoBehaviour
     private bool allowDuplicateFactionTroopTypes;
 
     [SerializeField]
-    private List<CharacterFaction> factions;
+    private CharacterFaction[] factions;
     [SerializeField]
-    private List<CharacterCombatType> troopTypes;
+    private CharacterCombatType[] troopTypes;
     [SerializeField]
-    private List<HeroCharacter> heroes;
+    private HeroCharacter[] heroes;
+
     // Start is called before the first frame update
     void Start()
     {
+        factions = Resources.LoadAll<CharacterFaction>("Data/Factions");
+        troopTypes = Resources.LoadAll<CharacterCombatType>("Data/Troop Types");
+        heroes = Resources.LoadAll<HeroCharacter>("Data/Heroes");
+        Debug.Log("Loading of game assets completed.");
+
+    }
+
+    private void GenerateRandomHeroes ()
+    {
+        throw new NotImplementedException();
+    }
+    private void GenerateRandomFactions ()
+    {
+        throw new NotImplementedException();
     }
 
     // Update is called once per frame
--- Assets/Scripts/Scriptables/Characters/HeroCharacter.cs
+++ Assets/Scripts/Scriptables/Characters/HeroCharacter.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Hero Character", menuName = " Hero persona", order = 3)]
+[CreateAssetMenu(fileName = "Hero Character", menuName = "Hero persona", order = 3)]
 public class HeroCharacter : ScriptableObject
 {
     [HideInInspector]
